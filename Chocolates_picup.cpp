71a03b97aec65fa4afb06d3e0f6a8e03f615b923
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-04-26 16:46:20 +0530

    Create Chocolates_picup.cpp

---
+++ Chocolates_picup.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int fun(int i , int j1 , int j2 , int n , int m , vector<vector<int>>& grid ,  vector<vector<vector<int>>>&dp){
+         if(j1 < 0 || j2 < 0 || j1 >= m || j2 >= m) return -1e8;
+         
+         if(i == n-1){
+             if( j1 == j2) return grid[i][j1];
+             else return grid[i][j1] + grid[i][j2];
+         }
+         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+         
+         int maxi = -1e8;
+         for(int dj1 = -1 ; dj1 <= +1 ; dj1++){
+             for(int dj2 = -1 ; dj2 <= +1 ; dj2++){
+                 int value = 0;
+                 if(j1 == j2) value = grid[i][j1] + fun(i+1 , j1 + dj1 , j2 + dj2 , n , m , grid , dp);
+                 else value = grid[i][j1]+grid[i][j2] + fun(i+1 , j1 + dj1 , j2 + dj2 , n , m , grid , dp);
+                 maxi = max(maxi , value);
+             }
+         }
+         return dp[i][j1][j2] = maxi;
+    }
+public:
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        vector<vector<vector<int>>>dp(n , vector<vector<int>>(m , vector<int>(m , -1)));
+        return fun(0 , 0 , m-1 , n , m , grid , dp);
+    }
+};
