23f637dc5f51b1dda4091feb7082dfd44eff0548
Roshan Raturi <77711686+roshan07273@users.noreply.github.com>
2023-08-19 16:10:39 +0530

    Create Prime_number.cpp

---
+++ Prime_number.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int isPrime(int n){
+        int cnt = 0;
+        for(int i = 1 ; i * i <= n ; i++){
+            if(n % i == 0){
+                cnt ++;
+                if( (n / i ) != i){
+                    cnt++;
+                }
+            }
+        }
+        if(cnt == 2) return 1;
+        return 0;
+    }
+};
